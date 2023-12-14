5cce33606a14837d73df7fc425726962bb1f6ea7
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-14 20:17:59 +0800

    configure bufferline mapping

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,8 @@
+return {
+  "akinsho/bufferline.nvim",
+  keys = {
+    { "<tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Navigate to next buffer" },
+    { "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Navigate to previous buffer" },
+    { "<leader>6", "<cmd>BufferLineCloseOthers<cr>", desc = "close other buffers" },
+  },
+}
