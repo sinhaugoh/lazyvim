8f1a71d927401158c37271c0bcf4a22dbe16443a
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-15 02:05:31 +0800

    change theme to have transparent background

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,23 @@
+return {
+  { "folke/tokyonight.nvim", opts = {
+    transparent = true,
+  } },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
