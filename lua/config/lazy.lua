2247c4abd3aa76de678501fe39727610db7c68e4
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-15 14:53:20 +0800

    remove project plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "plugins" },
   },
