bc4f83d27cf654a6993f3dc5bb552b84b2dd6770
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-18 00:59:01 +0800

    integrate DAP

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.ui.alpha" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,6 +3,9 @@ return {
   opts = {
     ensure_installed = {
       "html-lsp",
+      -- "node-debug2-adapter",
+      "js-debug-adapter",
+      -- "chrome-debug-adapter",
     },
   },
 }
