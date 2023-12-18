c5c5925696d22cc11c7c85ee39c6def392d7e3a6
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-18 23:16:13 +0800

    add color preview for rgba and hsl(this is not working)

---
+++ lua/plugins/nvim-colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  "NvChad/nvim-colorizer.lua",
+  opts = {
+    user_default_options = {
+      tailwind = true,
+      css = true,
+      sass = { enable = false, parsers = { "css" } },
+    },
+  },
+}
