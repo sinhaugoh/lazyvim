c2b320f1d9554b23683b01739be8ffab3230c9e8
sinhaugoh <sinhaugoh456@hotmail.com>
2023-12-22 00:49:19 +0800

    manually configure typescript (replace typescript.nvim to typescript-tools.nvim

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,9 +1,9 @@
 return {
   "akinsho/bufferline.nvim",
   keys = {
-    { "<tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Navigate to next buffer" },
-    { "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Navigate to previous buffer" },
-    { "<leader>6", "<cmd>BufferLineCloseOthers<cr>", desc = "close other buffers" },
+    { "<tab>", "<cmd>BufferLineCycleNext<cr>", desc = "Navigate to next buffer", mode = "n" },
+    { "<S-tab>", "<cmd>BufferLineCyclePrev<cr>", desc = "Navigate to previous buffer", mode = "n" },
+    { "<leader>6", "<cmd>BufferLineCloseOthers<cr>", desc = "close other buffers", mode = "n" },
   },
   opts = {
     options = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,6 +9,10 @@ return {
       { "gr", "<cmd>Telescope lsp_references show_line=false<cr>", desc = "Go to reference", silent = true }
     keys[#keys + 1] = { "K", false }
     keys[#keys + 1] = { "<leader>k", vim.diagnostic.open_float, desc = "Line Diagnostics" }
+    keys[#keys + 1] = { "<leader>cO", "<cmd>TSToolsOrganizeImports<cr>", desc = "Sorts and removes unused imports" }
+    keys[#keys + 1] =
+      { "<leader>cI", "<cmd>TSToolsAddMissingImports<cr>", desc = "adds imports for all statements that lack one" }
+    keys[#keys + 1] = { "<leader>cR", "<cmd>TSToolsRenameFile<cr>", desc = "Rename file" }
   end,
   opts = {
     diagnostics = {
