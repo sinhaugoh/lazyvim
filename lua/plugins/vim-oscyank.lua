53e5a0d12e7242ec080f50de0b1d6acc45bfac6e
sinhaugoh <sinhaugoh456@hotmail.com>
2024-02-21 18:14:49 +0000

    chore: add mapping for osc52 mapping

--- lua/plugins/vim-oscyank.lua
+++ lua/plugins/vim-oscyank.lua
@@ -3,7 +3,9 @@ return {
   -- setup: https://jdhao.github.io/2021/01/05/nvim_copy_from_remote_via_osc52/
   "ojroques/vim-oscyank",
   keys = {
-    { "<leader>y", "<Plug>OSCYankVisual", mode = { "n", "x" }, desc = "yank to system clipboard using ANSI OSC52 sequence (for ssh)" },
+    { "<leader>y",  "<Plug>OSCYankVisual",   mode = { "x" }, desc = "yank to system clipboard using ANSI OSC52 sequence (for ssh)" },
+    { "<leader>y",  "<Plug>OSCYankOperator", mode = { "n" }, desc = "yank to system clipboard using ANSI OSC52 sequence (for ssh)" },
+    { "<leader>yy", "<leader>y_",            remap = true,   mode = { "n" },                                                       desc = "yank to system clipboard using ANSI OSC52 sequence (for ssh)" },
   },
   config = function() end
 }
